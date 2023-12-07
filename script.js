dbd133e4f21db8898ea5111b26bd3f2df6f4d44d
Ryank2004 <ryankorver04@gmail.com>
2023-12-08 00:56:28 +0100

    overzetten

---
+++ script.js
@@ -0,0 +1,21 @@
+const offeredBtn = document.getElementById('offered'); /* gebruik getElementById als je class wilt selecterem, alleen elementen werken met queryselector*/
+const wantedBtn = document.getElementById('wanted');  /*niet vergeten een id bij je class te zetten in html*/
+const offeredList = document.getElementById('card-list-offered');
+const wantedList = document.getElementById('card-list-wanted');
+
+
+offeredBtn.addEventListener('click', function() {
+    offeredBtn.classList.add("active")
+    wantedBtn.classList.remove("active")
+    
+    offeredList.classList.add('active')
+    wantedList.classList.remove('active')
+})
+
+wantedBtn.addEventListener('click', function() {
+    wantedBtn.classList.add("active")
+    offeredBtn.classList.remove("active")
+    
+    offeredList.classList.remove('active')
+    wantedList.classList.add('active')
+})
